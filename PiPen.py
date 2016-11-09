179cb71bddc830554a512644e73389795df9aa12
Edward Digby <edward@ejdigby.com>
2016-11-09 20:11:52 +0000

    Commented Code

--- PiPen.py
+++ PiPen.py
@@ -1,40 +1,49 @@
-#PiPen
+#PiPen - Pi Code
+
+#Import Libraries
 from sense_hat import sense_hat
 import requests
 import pygame
 
+#Setup Colours
+# Colours: Red, Yellow, Green, Light Blue, Dark blue, pink, purple, black
 colour = 0
 colours = [[255, 0, 0], [255, 255, 0], [0, 255, 0], [0, 255, 255], [0, 0, 255], [128, 0, 255], [255, 0, 191], [255,255,255]]
 coloursHex = ["#ff0000","#ffff00", "#00ff00", "#00ffff", "#0000ff", "#8000ff", "#ff00bf", "#000000" ]
-# Colours: Red, Yellow, Green, Light Blue, Dark blue, pink, purple, black
 
+#Initialize pygame and sense
 pygame.init()
 pygame.display.set_mode((640, 480))
 sense = SenseHat()
 
+#Define Send Function
 def send(data):
 	r = requests.post('http://localhost:8080', data)
 
 
+#Draw colours on matrix
 for i in range(0, len(colours) - 1):
 	sense.set_pixel(i, 3, colours[i])
 
-
 while True:
+	#Get Accelerometer Data
     x, y = sense.get_accelerometer_raw().values()
 
     x=round(x, 0)
-    y=round(y, 0)
-
-    print("x=%s, y=%s" % (x, y))
-    send({"x":x, "y":y, "colour": coloursHex[colour]})
+    y=round(y, 0) 
 
     for event in pygame.event.get():	
     	if event.type == KEYDOWN:
+    		#When pushed right, change colour to the right by 1
             if event.key == K_RIGHT and colour < 7:
                 colour += 1
+            #When pushed left, change colour to the right by 1
             elif event.key == K_LEFT and colour > 0:
             	colour -= 1
 
+  
+    print("x=%s, y=%s, color=%s" % (x, y, coloursHex[colour]))
+    #Send latest data
+    send({"x":x, "y":y, "colour": coloursHex[colour]})
 
   # send({"x":1, "y":2, "colour": coloursHex[colour]})
--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,6 @@
+//Import Libraries and setup server
 var express = require("express")
 var bodyParser = require('body-parser')
-
-
 var app = express()
 var server = require('http').createServer(app);
 var port = 8080
@@ -9,24 +8,26 @@ var io = require('socket.io').listen(server);
 app.use(bodyParser());
 
 
+//Listen on port 8080
 server.listen(port)
-console.log(0, "Server Started On Port " + port)
+console.log("Server Started On Port " + port)
 
 
+//Define POST route for /
 app.post('/', function(req, res){
 	console.log(1, "Request for /")
 	//Handle input from the pi
-	console.log(req.body)
 
-	console.log(req.body.x)
-
+	//Define received data
 	x = req.body.x
 	y = req.body.y
 	colour = req.body.colour	
 
+	//Emit data to html
 	data = req.body
 	io.emit('data', data)
 	
 });
 
+//Define GET route for /
 app.use('/', express.static(__dirname + "/public/"))
